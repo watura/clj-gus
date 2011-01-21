f200d47e412e3c624dc02fd3ea44b6ad4187daa7
NISHIMOTO Wataru <watura@g91.org>
2011-01-21 16:51:01 +0900

    initialized

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject gus "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [org.apache.commons/commons-email "1.2"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :main twimail.core)
---
+++ test/gus/test/core.clj
@@ -0,0 +1,6 @@
+(ns gus.test.core
+  (:use [gus.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
