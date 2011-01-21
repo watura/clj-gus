e223f4b905bb66b033440513c401fb8972ed0978
NISHIMOTO Wataru <watura@g91.org>
2011-01-21 20:34:54 +0900

    shortener expand analytics

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject gus "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Library for Google URL Shortener"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [org.apache.commons/commons-email "1.2"]]
+		 [com.twinql.clojure/clj-apache-http "2.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  :main twimail.core)
+  :main gus.core)
--- src/gus/core.clj
+++ src/gus/core.clj
@@ -1,1 +1,20 @@
 (ns gus.core)
+(require ['clojure.contrib.http.agent :as 'agent])
+(require ['clojure.contrib.json :as 'json])
+
+
+(def gus-url "https://www.googleapis.com/urlshortener/v1/url?key=")
+(defn shortener [url]
+  (json/read-json (agent/string (agent/http-agent
+				 (str gus-url gus-key)
+				 :method "POST"
+				 :body (str "{\"longUrl\":\"" url "\"}" )
+				 :headers {"Content-Type" "application/json"}))))
+
+(defn expand [url]
+  (json/read-json (agent/string (agent/http-agent
+				 (str gus-url gus-key "&shortUrl=" url)))))
+(defn analytics [url]
+    (json/read-json (agent/string (agent/http-agent
+				   (str gus-url gus-key "&projection=FULL&shortUrl=" url)))))
+
